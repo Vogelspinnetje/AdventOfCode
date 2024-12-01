b9d21f9f9bec61e102d4669d57b2bde721904897
yesse monkou <monkou.yesse@gmail.com>
2024-12-01 06:12:44 +0100

    Code waarmee de opdracht is behaald

--- 2024/AoC_01_2024.py
+++ 2024/AoC_01_2024.py
@@ -1,8 +1,22 @@
-def AoC_01_2024(datasteam) -> tuple[int,int]:
-    return 0,0
+def AoC_01_2024(datastream1, datastream2) -> tuple[int,int]:
+    datastream1.sort()
+    datastream2.sort()
+
+    antwoord = 0
+    gelijkenis = 0
+
+    for i in range(len(datastream1)):
+        antwoord += abs(datastream1[i] - datastream2[i])
+        gelijkenis += datastream2.count(datastream1[i]) * datastream1[i]
+
+    return antwoord,gelijkenis
 
 if __name__ == "__main__":
+    invoerL = []
+    invoerR = []
     with open("AoC_01_2024.txt", "r") as fh:
-        invoer = [regel.strip() for regel in fh]
+        for regels in fh:
+            invoerL.append(int(regels.split("   ")[0].strip()))
+            invoerR.append(int(regels.split("   ")[1].strip()))
 
-    print(AoC_01_2024(invoer))
+    print(AoC_01_2024(invoerL, invoerR))
