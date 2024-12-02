c68e9dc1b25787458a89cfdb7d72a07e7bac4fcd
yesse monkou <monkou.yesse@gmail.com>
2024-12-02 20:50:53 +0100

    Startcode voor dag 3

---
+++ 2024/AoC_03_2024.py
@@ -0,0 +1,9 @@
+def AoC_03_2024(datastream: list) -> tuple[0, 0]:
+    return 0,0
+
+
+if __name__ == "__main__":
+    with open("AoC_03_2024.txt", "r") as fh:
+        invoer = [lines for lines in fh]
+
+    print(AoC_03_2024(invoer))
