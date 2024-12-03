244c6fd2d44d7b929e85fb46665682266c6d5924
yesse monkou <monkou.yesse@gmail.com>
2024-12-03 18:27:13 +0100

    Deel 2 werkend

--- 2024/AoC_03_2024.py
+++ 2024/AoC_03_2024.py
@@ -2,12 +2,31 @@ import re
 
 def AoC_03_2024(datastream: list) -> tuple[0, 0]:
     antwoord: int = 0
+    antwoord2: int = 0
+
+    stoppen: bool = False
+
     for regels in datastream:
-        sommen: list = re.findall(r"mul\(\d{1,3},\d{1,3}\)", "".join(regels))
+        sommen: list = re.findall(r"mul\(\d{1,3},\d{1,3}\)|don\'t\(\)|do\(\)", "".join(regels))
         for som in sommen:
-            getallen = re.findall(r"\d{1,3}", som)
+            if som == "don\'t()":
+                print("---"*20)
+                stoppen = True
+                continue
+            
+            elif som == "do()":
+                print("***"*20)
+                stoppen = False
+                continue
+            
+            
+            getallen: list[str] = re.findall(r"\d{1,3}", som)
             antwoord += int(getallen[0]) * int(getallen[1])
-    return antwoord,0
+            
+            if not stoppen:
+                antwoord2 += int(getallen[0]) * int(getallen[1])
+
+    return antwoord, antwoord2
 
 
 if __name__ == "__main__":
