6b7701e8e68c6dd67bf49d158a84c030c66bc461
yesse monkou <monkou.yesse@gmail.com>
2024-12-03 07:21:44 +0100

    Deel van deel2. Nog niet werkend.

--- 2024/AoC_03_2024.py
+++ 2024/AoC_03_2024.py
@@ -2,15 +2,34 @@ import re
 
 def AoC_03_2024(datastream: list) -> tuple[0, 0]:
     antwoord: int = 0
+    antwoord2: int = 0
 
     for regels in datastream:
         sommen: list = re.findall(r"mul\(\d{1,3},\d{1,3}\)", "".join(regels))
+        sommen_pos: list = re.finditer(r"mul\(\d{1,3},\d{1,3}\)", "".join(regels))
+        deadzone_zoeken: list = re.finditer(r"don\'t\(\).{1,}?do\(\)", regels)
+        deadzone: list = [[int(zones.start()), int(zones.end())] for zones in deadzone_zoeken]
+        print(deadzone)
 
-        for som in sommen:
+        for som, pos in zip(sommen, sommen_pos):
+            start_pos: int = int(pos.start())
+            mag_rekenen: bool = True
+
+            for zones in deadzone:
+                print(f"Controleer zone: {zones} met start_pos: {start_pos}")
+                if zones[0] < start_pos < zones[1]:
+                    print(f"start_pos {start_pos} valt in zone {zones}")
+                    mag_rekenen = False
+                    break
+
+
             getallen = re.findall(r"\d{1,3}", som)
             antwoord += int(getallen[0]) * int(getallen[1])
+            if mag_rekenen:
+                antwoord2 += int(getallen[0]) * int(getallen[1])
+            break
 
-    return antwoord,0
+    return antwoord,antwoord2
 
 
 if __name__ == "__main__":
