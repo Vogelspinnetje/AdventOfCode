bfdd4a882e1a7f89486a4d270bb33aba160740f3
Vogelspinnetje <monkou.yesse@gmail.com>
2025-07-11 18:56:39 +0200

    Part 1

--- 2020/src/AoC_01_2020.java
+++ 2020/src/AoC_01_2020.java
@@ -1,2 +1,31 @@
+import java.io.IOException;
+import java.util.List;
+
 public class AoC_01_2020 {
+    public static void main(String[] args) throws IOException {
+        List<String> input = Util.readInput("inputs/AoC_01_2020.txt");
+        int comparator;
+        int base;
+        int answer1 = 0;
+        boolean break_loop = false;
+
+        for (int i = 0; i < input.size(); i++){
+            if (break_loop) break;
+            base = Integer.parseInt(input.get(i));
+
+            for (int j = i; j < input.size(); j ++){
+                if (i == j) continue;
+                if (break_loop) break;
+
+                comparator = Integer.parseInt(input.get(j));
+
+                if (base + comparator == 2020) {
+                    answer1 = base * comparator;
+                    break_loop = true;
+                }
+            }
+        }
+
+        System.out.println(answer1);
+    }
 }
